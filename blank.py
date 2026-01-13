fa5fc45f4c6220b65fb407efa21844df164bd207
KRISHNA SAI ALLU <150152859+krsna1206@users.noreply.github.com>
2026-01-13 11:06:39 +0530

    blank

--- blank.py
+++ blank.py
@@ -1,13 +1,10 @@
 class Main:
-    # In Python, we use 'self' as the first argument for instance methods
     def add(self, a, b):
         return a + b
     
     def run(self):
-        # We call the method using self.method_name
         print(self.add(5, 4))
 
-# To run the code:
 if __name__ == "__main__":
     obj = Main()
     obj.run()
